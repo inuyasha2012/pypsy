1eed348fd3a8b9c27ea72d476ab2844113468eb8
inuyasha2012 <chinachrist@163.com>
2018-01-26 12:28:32 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from distutils.core import setup
 setup(
     name='pypsy',
     version='0.01',
-    packages=['psy', 'psy.fa', 'psy.cat', 'psy.cdm', 'psy.mirt', 'psy.utils', 'psy.settings', 'demo'],
+    packages=['psy', 'psy.fa', 'psy.cat', 'psy.cdm', 'psy.ctt', 'psy.sem',
+              'psy.mirt', 'psy.utils', 'psy.settings', 'demo'],
     url='',
     license='',
     author='inuyasha',
